e32fc69863a84146e565e31c8b54935eaa5f8007
Elmin <admin@scrysoft-studios.com>
2021-03-31 20:06:32 +0200

    Login Rough Style

--- htdocs/admin.php
+++ htdocs/admin.php
@@ -34,15 +34,16 @@ $pageSwitch = new IncludeSwitch(new AdminPages());
     <meta name="msapplication-TileImage" content="/mstile-144x144.png">
 </head>
 <body>
-<?php
-$actionSwitch->include(AdminActions::NONE); // Include action switch
-$pageSwitch->include(AdminPages::LOGIN); // Include page
-?>
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<!-- Javascript -->
-<script src="mockup/res/js/navbar.js"></script>
+    <?php
+        $actionSwitch->include(AdminActions::NONE); // Include action switch
+        $pageSwitch->include(AdminPages::LOGIN); // Include page
+    ?>
+    
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <!-- Javascript -->
+    <script src="mockup/res/js/navbar.js"></script>
 </body>
 </html>
--- htdocs/admin/template/LoginTemplate.php
+++ htdocs/admin/template/LoginTemplate.php
@@ -1,5 +1,27 @@
-<form method="post" action="<?="admin.php?page=".$redirectTarget."&action=".AdminActions::LOGIN?>">
-    <input type="text" name="username" placeholder="Username...">
-    <input type="password" name="password" placeholder="Passwort...">
-    <input type="submit" value="Login">
-</form>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <img src="../htdocs/mockup/media/bootstrap-solid.svg" class="logo-login" alt="...">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <form method="post" action="<?="admin.php?page=".$redirectTarget."&action=".AdminActions::LOGIN?>">
+                <div class="form-group">
+                    <input class="form-control" type="text" name="username" placeholder="Username...">
+                </div>
+                <div class="form-group">
+                    <input class="form-control" type="password" name="password" placeholder="Passwort...">
+                </div>
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Benutzerdaten merken</label>
+                </div>
+                <hr>
+                <div class="form-group">
+                    <input class="btn btn-primary btn-block" type="submit" value="Login">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
