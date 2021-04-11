0d89b45e146ff43185ff6bbe4662943a144ec75e
ToadsworthLP <fiery.toadsworth@gmail.com>
2021-04-11 12:00:22 +0200

    Fixed renamed database column reference in HomepageGateway

--- htdocs/homepage/gateway/HomepageGateway.php
+++ htdocs/homepage/gateway/HomepageGateway.php
@@ -34,8 +34,8 @@ class HomepageGateway
         $sql = "SELECT b.id AS 'block_id', block_order, b.filename AS 'block_type', a.name AS 'attribute', ta.text AS 'text', f.id AS 'file' FROM block
                 JOIN blocktype b on block.type = b.id
                 JOIN attribute a ON block.id = a.block
-                LEFT JOIN text_attribute ta on a.id = ta.id
-                LEFT JOIN file_attribute fa on a.id = fa.id
+                LEFT JOIN text_attribute ta on a.id = ta.parent
+                LEFT JOIN file_attribute fa on a.id = fa.parent
                 LEFT JOIN file f on fa.file = f.id
                 WHERE site = :site
                 ORDER BY block_order, b.id";
