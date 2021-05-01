ea4f82472943b30700fb083f0f1c3c10e073d65b
ToadsworthLP <fiery.toadsworth@gmail.com>
2021-05-01 18:29:46 +0200

    Renamed and added comments to ImageUploadUtility

--- htdocs/admin/action/UploadImageAction.php
+++ htdocs/admin/action/UploadImageAction.php
@@ -3,7 +3,7 @@ include_once "admin/utility/Alert.php";
 include_once "admin/utility/AlertType.php";
 include_once "admin/utility/RequirePermissions.php";
 include_once "admin/config/PermissionConfig.php";
-include_once "admin/utility/ImageUtility.php";
+include_once "admin/utility/ImageUploadUtility.php";
 include_once "common/model/File.php";
 include_once "common/gateway/FileGateway.php";
 include_once "lib/phpThumb/phpthumb.class.php";
@@ -15,10 +15,10 @@ if(isset($_FILES["upload"]) && $_FILES["upload"]["error"] === UPLOAD_ERR_OK) {
     $file->filename = $_FILES["upload"]["name"];
     $file->create();
 
-    $imgUtil = new ImageUtility($_FILES["upload"]["tmp_name"], $file->getID());
+    $imgUtil = new ImageUploadUtility($_FILES["upload"]["tmp_name"], $file->getID());
 
     if($imgUtil->isValidImage()) {
-        if($imgUtil->upload($file->getID(), true, 256, 256) === false) {
+        if($imgUtil->upload(true, 256, 256) === false) {
             $file->delete(); // Delete file entry if the upload failed
             $error = new Alert(AlertType::WARNING, "Das Bild konnte nicht hochgeladen werden.", true);
             $error->post();
--- htdocs/admin/utility/ImageUploadUtility.php
+++ htdocs/admin/utility/ImageUploadUtility.php
@@ -2,15 +2,15 @@
 include_once "common/config/PathConfig.php";
 include_once "lib/phpThumb/phpthumb.class.php";
 
-class ImageUtility
+class ImageUploadUtility
 {
     private string $tempPath;
     private string $filename;
 
     /**
-     * ImageUtility constructor.
-     * @param string $tempPath
-     * @param string string $filename
+     * Creates a new instance of the image upload utility
+     * @param string $tempPath The temp path the image was saved to
+     * @param string $filename The actual destination file name of the uploaded file
      */
     public function __construct(string $tempPath, string $filename)
     {
@@ -18,6 +18,10 @@ class ImageUtility
         $this->filename = $filename;
     }
 
+    /**
+     * Checks whether the uploaded file is a valid image file.
+     * @return bool True if it is valid, otherwise false
+     */
     public function isValidImage() : bool {
         $info = getimagesize($this->tempPath);
         if ($info != false && (($info[2] === IMAGETYPE_GIF) || ($info[2] === IMAGETYPE_JPEG) || ($info[2] === IMAGETYPE_PNG))) {
@@ -27,8 +31,16 @@ class ImageUtility
         return false;
     }
 
-    public function upload(string $filename, bool $generateThumbnail = false, int $thumbnailWidth = -1, int $thumbnailHeight = -1) : bool {
-        $targetPath = PathConfig::IMAGE_PATH_FULL.$filename;
+    /**
+     * Moves the uploaded file from the temp path to its final destination.
+     * Optionally generates a thumbnail with the given dimensions.
+     * @param bool $generateThumbnail Whether to generate a thumbnail for the newly uploaded image
+     * @param int $thumbnailWidth The width of the generated thumbnail in pixels
+     * @param int $thumbnailHeight The height of the generated thumbnail in pixels
+     * @return bool True if the operation was successful, otherwise false
+     */
+    public function upload(bool $generateThumbnail = false, int $thumbnailWidth = -1, int $thumbnailHeight = -1) : bool {
+        $targetPath = PathConfig::IMAGE_PATH_FULL.$this->filename;
         $success = move_uploaded_file($this->tempPath, $targetPath);
         if($generateThumbnail) $success &= $this->generateThumbnail($thumbnailWidth, $thumbnailHeight);
 
