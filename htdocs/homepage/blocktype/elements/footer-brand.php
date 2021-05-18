f211d7f976fa30121dbe18411a53b51efc413e29
ToadsworthLP <fiery.toadsworth@gmail.com>
2021-05-18 12:05:05 +0200

    Fixed footer logo

--- htdocs/homepage/blocktype/elements/footer-brand.php
+++ htdocs/homepage/blocktype/elements/footer-brand.php
@@ -1,2 +1,2 @@
-<img src="mockup/media/bootstrap-solid.svg" class="img-fluid" alt="...">
+<img src="<?= PathConfig::IMAGE_PATH_FULL.Settings::get(SettingsConfig::FOOTER_LOGO) ?>" class="img-fluid" alt="...">
 <p><?= Settings::get(SettingsConfig::FOOTER_DESCRIPTION) ?></p>
