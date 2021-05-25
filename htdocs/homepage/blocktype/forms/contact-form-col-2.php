a8e0b72c0aedcf3575cc56e6b0d7b3ce2a56d1eb
ToadsworthLP <fiery.toadsworth@gmail.com>
2021-05-25 15:46:56 +0200

    Fixed contact form

--- htdocs/homepage/blocktype/forms/contact-form-col-2.php
+++ htdocs/homepage/blocktype/forms/contact-form-col-2.php
@@ -1,25 +1,25 @@
 <section class="my-section">
     <div class="container">
-        <form>
+        <form method="post" action="index.php?action=<?= HomepageActions::SEND_MESSAGE ?>">
             <div class="row">
                 <div class="col-md-6">
                     <div class="form-group">
                         <label for="name" hidden>Name</label>
-                        <input id="name" class="form-control" type="text" placeholder="Name">
+                        <input id="name" class="form-control" type="text" name="name" placeholder="Name">
                     </div>
                     <div class="form-group">
                         <label for="email" hidden>E-Mail-Adresse</label>
-                        <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="E-Mail-Adresse">
+                        <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="E-Mail-Adresse">
                     </div>
                     <div class="form-group">
                         <label for="topic" hidden>Betreff</label>
-                        <input id="topic" class="form-control" type="text" placeholder="Betreff">
+                        <input id="topic" class="form-control" type="text" name="subject" placeholder="Betreff">
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="form-group">
                         <label for="message" hidden>Example textarea</label>
-                        <textarea class="form-control" id="message" rows="3"></textarea>
+                        <textarea class="form-control" id="message" name="message" rows="3"></textarea>
                     </div>
                 </div>
                 <div class="col-12">
