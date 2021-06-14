84cca4dcaebffddc11b363e9d92b040602858d6b
StephanWeinlinger <stephan.weinlinger@gmail.com>
2021-06-14 12:13:38 +0200

    Changed Slider Color-Options

--- htdocs/homepage/blocktype/elements/slider-img.php
+++ htdocs/homepage/blocktype/elements/slider-img.php
@@ -2,11 +2,11 @@
     <img src="<?=$slide->imagePath?>" class="d-block w-100" alt="...">
 <?php endif; ?>
 <div class="carousel-caption d-none d-md-block slider-heading">
-    <h2><?=$slide->title?></h2>
-    <hr class="heading-hairline">
-    <h4><?=$slide->text?></h4>
+    <h2 style="color: <?=$slide->buttonTextColor?>"><?=$slide->title?></h2>
+    <hr class="heading-hairline" style="background-color: <?=$slide->buttonHoverColor?>">
+    <h4 style="color: <?=$slide->buttonTextColor?>"><?=$slide->text?></h4>
     <?php if($slide->buttonText !== ""): ?>
         <!-- color: Schriftfarbe, onMouseover: buttonhoverfarbe, onMouseOut: buttonhintergrundfarbe -->
-        <a class="btn btn-primary" href="<?=$slide->buttonLink?>" role="button" style="color: <?=$slide->buttonTextColor?>; background-color: <?=$slide->buttonColor?>" onMouseOver="this.style.backgroundColor='<?=$slide->buttonHoverColor?>'" onMouseOut="this.style.backgroundColor='<?=$slide->buttonColor?>'"><?=$slide->buttonText?></a>
+        <a class="btn btn-primary" href="<?=$slide->buttonLink?>" role="button" style="color: <?=$slide->buttonTextColor?>; background-color: <?=$slide->buttonColor?>; border-color: <?=$slide->buttonHoverColor?>" onMouseOver="this.style.backgroundColor='<?=$slide->buttonHoverColor?>'" onMouseOut="this.style.backgroundColor='<?=$slide->buttonColor?>'"><?=$slide->buttonText?></a>
     <?php endif; ?>
 </div>
